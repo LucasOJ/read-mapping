48fabf43957dfb9a34a27779b2e42d890f8c1df6
LucasOJ <lucasodowdjones@gmail.com>
2025-06-24 19:53:01 +0100

    Add FASTQ parsing

---
+++ src/fastq_parsing.rs
@@ -0,0 +1,16 @@
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+pub fn read_fastq(file_path: &str) -> impl Iterator<Item = String> {
+    let file = File::open(file_path).expect("FASTQ failed to open");
+    let reader = BufReader::new(file);
+
+    // TODO: Make more robust - assumes that the 1st, 5th, 9th, 13th lines are the reads 
+    let reads = reader
+        .lines() // Get an iterator over lines
+        .filter_map(Result::ok) // Ignore any errors
+        .skip(1)
+        .step_by(4);
+
+    return reads;
+}
