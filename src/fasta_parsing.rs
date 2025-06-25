185109533e88ec5d9bb2d16d427ede365445d7c4
LucasOJ <lucasodowdjones@gmail.com>
2025-06-25 18:55:42 +0100

    Modify FASTA parser not to add sentinel

--- src/fasta_parsing.rs
+++ src/fasta_parsing.rs
@@ -1,11 +1,11 @@
 use std::fs::File;
-use std::io::{self, BufRead};
+use std::io::{BufReader, BufRead};
 
 pub fn read_fasta(file_path: &str) -> String {
     print!("Loading FASTA in {} to memory", file_path);
 
     let file = File::open(file_path).expect("FASTA failed to open");
-    let reader = io::BufReader::new(file);
+    let reader = BufReader::new(file);
 
     let lines: Vec<String> = reader
         .lines() // Get an iterator over lines
@@ -14,9 +14,9 @@ pub fn read_fasta(file_path: &str) -> String {
         .filter(|line| line.chars().next().unwrap() != '>')
         .collect();
     
-    let genome = lines.join("") + "$";
+    let genome = lines.join("");
 
-    println!("[DONE]");
+    println!(" [DONE]");
     println!("Genome: {}...", &genome[..30]);
 
     return genome;
