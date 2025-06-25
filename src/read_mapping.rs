7e8c377e7ac1fc734b58498c0811d3689d7aa11a
LucasOJ <lucasodowdjones@gmail.com>
2025-06-25 20:35:04 +0100

    Add forwards extension from the seed

--- src/read_mapping.rs
+++ src/read_mapping.rs
@@ -3,14 +3,19 @@ use std::{cmp::min, collections::HashMap};
 use crate::fm_index::FMIndex;
 
 pub struct ReadMapper {
-    // forwards_fm_index: FMIndex,
+    forwards_fm_index: FMIndex,
     reverse_fm_index: FMIndex,
     genome_length: usize,
 }
 
 impl ReadMapper {
     pub fn new(genome: &str) -> Self {
-        // let forwards_fm_index = FMIndex::new(&genome, 128, 128);
+
+        // TODO: Not nice having to reallocate `str` just to add the sentinel
+        let mut forwards_genome = String::from(genome);
+        forwards_genome.push('$');
+
+        let forwards_fm_index = FMIndex::new(&forwards_genome, 128, 128);
 
         let mut reversed_genome: String = genome.chars().rev().collect();
         reversed_genome.push('$');
@@ -18,7 +23,7 @@ impl ReadMapper {
         let reverse_fm_index = FMIndex::new(&reversed_genome, 128, 128);
 
         return ReadMapper {
-            // forwards_fm_index,
+            forwards_fm_index,
             reverse_fm_index,
             genome_length: genome.len() + 1, // +1 for sentinel
         };
@@ -57,7 +62,7 @@ impl ReadMapper {
             let reverse_seed = &reversed_read[reverse_seed_start_index..reverse_seed_end_index];
 
             /*
-             * We check for the seed in the reverse genome as often if there is a match for the 
+             * We check for the seed in the reverse genome as often if there is a match for the
              * first seed (which happens often), then we can extend to the rest of the read without
              * consulting the forwards_fm_index
              */
@@ -71,7 +76,7 @@ impl ReadMapper {
             // Get the read extension from where the seed ends
             let reverse_extension = &read[seed_end_index..];
 
-            let mut extension_length_map: HashMap<_, _> = reverse_lookup_results
+            let mut match_length_map: HashMap<_, _> = reverse_lookup_results
                 .map(|lookup_result| {
                     let reverse_genome_pos =
                         self.reverse_fm_index.get_genome_position(lookup_result);
@@ -81,12 +86,12 @@ impl ReadMapper {
 
                     /*
                      * Converts to a genome_pos in the forwards genome.
-                     * 
+                     *
                      * `self.genome_length - reverse_genome_pos` is classic index reversal
-                     * 
-                     * `-seed_length` since the beginning of the seed in the reverse genome 
+                     *
+                     * `-seed_length` since the beginning of the seed in the reverse genome
                      * is the end of the seed in the forwards genome
-                     * 
+                     *
                      * `-1` since the sentinel is at the end of the genomes in both cases
                      */
                     let genome_pos = self.genome_length - reverse_genome_pos - seed_length - 1;
@@ -95,9 +100,53 @@ impl ReadMapper {
                 })
                 .collect();
 
-            // TODO: If length of forwards continuation != 0, try adding forwards
+            let forwards_extension = &reversed_read[read.len() - seed_start_index..read.len()];
 
-            return extension_length_map
+            /*
+             * Only consider forwards_fm_index if we have something to gain.
+             * Note that the forwards_extension_length can be at most the seed_length,
+             * as if it was longer then the previous seed would have matched in the 
+             * reverse_fm_index lookup.
+             */ 
+            if forwards_extension.len() > 0 {
+                let forwards_seed = &read[seed_start_index..seed_end_index];
+
+                let forwards_lookup_results = self.forwards_fm_index.lookup(forwards_seed);
+
+                for lookup_result in forwards_lookup_results {
+                    let forwards_genome_pos =
+                        self.forwards_fm_index.get_genome_position(lookup_result);
+                    let forwards_extension_length = self
+                        .forwards_fm_index
+                        .count_extension_matches(lookup_result, forwards_extension);
+
+                    // The position where the left-extension in the forwards genome begins
+                    let genome_pos = forwards_genome_pos - forwards_extension_length;
+
+                    /*
+                     * If we already have the length of a reverse match from the same seed extract it
+                     * 
+                     *       <--forwards_extension_length--|-----reverse_match_length------>
+                     *       <-----forwards_extension------|--seed--|--reverse_extension--->
+                     * ------|-----------------------------|--------|----------------------|------
+                     *       ^                             ^
+                     *    genome_pos              forwards_genome_pos
+                     * 
+                     * Should always be something here since the reverse lookup should populate it 
+                     * with at least the length of the seed. Hence, we do `unwrap` to panic if 
+                     * something has gone wrong
+                     */ 
+                    let reverse_match_length =
+                        match_length_map.remove(&forwards_genome_pos).unwrap();
+
+                    // TODO: Can there already be something at genome_pos?
+                    match_length_map
+                        .insert(genome_pos, forwards_extension_length + reverse_match_length);
+                }
+            }
+
+            // Return longest match
+            return match_length_map
                 .into_iter()
                 .max_by_key(|(_, length_ref)| *length_ref);
         }
@@ -114,14 +163,48 @@ mod tests {
     #[test]
     fn map_read() {
         let read_mapper = ReadMapper::new("ATACTTTATCAAATGTAAAAGTATCTCCTTCGTTTACGTCTAATTTTT");
+        
+        // |------| is used to highlight the matches between the reads and the genome
+
+        //                               |--|
         assert_eq!(read_mapper.map_read("ATAC", 4, 1), Some((0, 4)));
+
         assert_eq!(
+            //                    |----------------|
             read_mapper.map_read("ATACTTTATCAAATGTAA", 5, 2),
             Some((0, 18))
         );
+
+        //                               |------------|
         assert_eq!(read_mapper.map_read("ATCAAATGTAAAAG", 7, 2), Some((7, 14)));
+
         assert_eq!(read_mapper.map_read("ATCAATTGTAAAA", 7, 2), None);
+
         assert_eq!(read_mapper.map_read("ATCCAATGTAAAAG", 4, 1), None);
+
+        assert_eq!(
+            //                     |---------------|
+            read_mapper.map_read("TTACTTTATCAAATGTAA", 5, 2),
+            Some((1, 17))
+        );
+        
+        assert_eq!(
+            //                      |--------------|
+            read_mapper.map_read("AAACTTTATCAAATGTAA", 5, 2),
+            Some((2, 16))
+        );
+
+        assert_eq!(
+            //                              |--------------|
+            read_mapper.map_read("GTATCTTCTACGTTTACGTCTAATTT", 7, 3),
+            Some((30, 16))
+        );
+
+        assert_eq!(
+            //                              |-----------|
+            read_mapper.map_read("GTATCTTCTACGTTTACGTCTAAATT", 7, 3),
+            Some((30, 13))
+        );
     }
 
     #[test]
